73ef642e6b4c74344a4a148bb732f545fa09d19e
Darwin Vaz <dvaz@purdue.edu>
2017-09-23 18:32:42 -0400

    Add files via upload

---
+++ Backup2/Controller.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Camera-Control/Mouse Look")]
+public class Controller : MonoBehaviour {
+
+	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+	public float sensitivityX;
+	public float sensitivityY;
+
+	public float minimumX = -360F;
+	public float maximumX = 360F;
+
+	public float minimumY = -60F;
+	public float maximumY = 60F;
+
+	float rotationY = 0F;
+	public float movementSpeed;
+	public GameObject fireBall;
+
+	private Rigidbody rb;
+	public int jumpForce;
+	float lastJump = 0;
+
+	void Update ()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			Instantiate (fireBall, new Vector3(transform.position.x, transform.position.y + 1F, transform.position.z), Quaternion.identity);
+		}
+		if(Input.GetKey(KeyCode.W)) {
+			transform.position += transform.forward * Time.deltaTime * movementSpeed;
+		}
+		if(Input.GetKey(KeyCode.S)) {
+			transform.position -= transform.forward * Time.deltaTime * movementSpeed;
+		}
+		if (Input.GetKey (KeyCode.Space) && Time.time - lastJump >= 1) {
+			rb.AddForce(new Vector3(0, jumpForce, 0));
+			lastJump = Time.time;
+		}
+		if (axes == RotationAxes.MouseXAndY)
+		{
+			float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+
+			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+		}
+		else if (axes == RotationAxes.MouseX)
+		{
+			transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+		}
+		else
+		{
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+
+			transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+		}
+	}
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		if (rb) 
+			rb.freezeRotation = true;
+	}
+}
---
+++ Backup2/FireballController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballController : MonoBehaviour {
+
+	public GameObject player;
+	float currentTime;
+
+	// Use this for initialization
+	void Start () {
+		currentTime = Time.time;
+		player = GameObject.FindGameObjectWithTag("player");
+		//ransform.position += player.transform.forward;
+		transform.rotation = player.transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.forward;
+		if (Time.time - currentTime >= 5F) {
+			Destroy (this.gameObject);
+		}
+	}
+}
