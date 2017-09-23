4a771e2770d1a197317aae783fc33b51e3c9f165
Hiten98 <hitenrathod98@gmail.com>
2017-09-23 16:55:46 -0400

    Button Code
    Add each behaviour to different buttons

---
+++ Button_Code/NewBehaviourScript3.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class NewBehaviourScript3 : MonoBehaviour {
+	public NewBehaviourScript3()
+	{
+	}
+	public int getNumber()
+	{
+		return ((int.Parse (text.text) + 1)%10);;
+	}
+	void Start () {
+		text = GetComponentsInChildren <Text> ()[0];
+		text.text = "3";
+		button = GetComponent<Button> ();
+		time = 0;
+		time1 = Time.time;
+	}
+	int time = 0;
+	Text text;
+	Button button;
+	float time1;
+	void change()
+	{
+		//time2 = Time.time;
+		if (time == 0) {
+			int no = ((int.Parse (text.text) + 1)%10);
+			text.text = no.ToString ();
+			Debug.Log (int.Parse (text.text));
+			time++;
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		button.onClick.AddListener(()=>change());
+		if (Time.time - time1 > 0.1)
+			time = 0;
+	}
+}
